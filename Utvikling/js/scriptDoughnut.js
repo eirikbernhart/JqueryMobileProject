27571bc8c23e84325936c42765f33ac914a00a16
stompalumpa <bereir15@student.westerdals.no>
2016-12-14 20:10:41 +0100

    Fikset litt på stilen

--- Utvikling/js/scriptDoughnut.js
+++ Utvikling/js/scriptDoughnut.js
@@ -1,26 +1,91 @@
-var ctx = document.getElementById('myDChart');
+
+
+
+var ctx = document.getElementById('myDChart').getContext("2d");
+ctx.canvas.width = 350;
+ctx.canvas.height = 350;
+
 
 var data = {
     labels: [
-        "Rent",
-        "Sosial",
-        "Food"
+       "1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7"
+		
     ],
     datasets: [
         {
-            data: [300, 50, 100],
+            data: [300, 50, 100, 30, 80, 45, 120],
             backgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
+                "#704c93",
+                "#8466a3",
+                "#997fb2",
+				"#AD99C1",
+				"#C1B2D1",
+				"#D6CCE0",
+				"#EAE5EF"
             ],
             hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
+                "#704c93",
+                "#8466a3",
+                "#997fb2",
+				"#AD99C1",
+				"#C1B2D1",
+				"#D6CCE0",
+				"#EAE5EF"
             ]
         }]
 };
+
+
+Chart.pluginService.register({
+			beforeRender: function (chart) {
+				if (chart.config.options.showAllTooltips) {
+					// create an array of tooltips
+					// we can't use the chart tooltip because there is only one tooltip per chart
+					chart.pluginTooltips = [];
+					chart.config.data.datasets.forEach(function (dataset, i) {
+						chart.getDatasetMeta(i).data.forEach(function (sector, j) {
+							chart.pluginTooltips.push(new Chart.Tooltip({
+								_chart: chart.chart,
+								_chartInstance: chart,
+								_data: chart.data,
+								_options : chart.options.tooltips,
+								_active: [sector]
+							}, chart));
+						});
+					});
+
+					// turn off normal tooltips
+					chart.options.tooltips.enabled = false;
+				}
+			},
+			afterDraw: function (chart, easing) {
+				if (chart.config.options.showAllTooltips) {
+					// we don't want the permanent tooltips to animate, so don't do anything till the animation runs atleast once
+					if (!chart.allTooltipsOnce) {
+						if (easing !== 1)
+							return;
+						chart.allTooltipsOnce = true;
+					}
+
+					// turn on tooltips
+					chart.options.tooltips.enabled = true;
+					Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
+						tooltip.initialize();
+						tooltip.update();
+						// we don't actually need this since we are not animating tooltips
+						tooltip.pivot();
+						tooltip.transition(easing).draw();
+					});
+					chart.options.tooltips.enabled = false;
+				}
+			}
+		})
 
 
 
@@ -28,6 +93,12 @@ var chartInstance = new Chart(ctx, {
 			type: 'doughnut',
 			data: data,
 			options: {
-        responsive: false
-    		}
-		});
+				showAllTooltips: true,
+        responsive: false,	
+		cutoutPercentage: 70
+	    
+				
+		}
+});
+
+
