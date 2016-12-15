0e7068b22d779ebf18d1e2974b9b4357f26fe03e
stompalumpa <bereir15@student.westerdals.no>
2016-12-16 00:07:04 +0100

    Da kan man hente data fra database, og få dette satt i i jquery mobile sin listview, med riktig css!:)

--- Utvikling/js/firebase.js
+++ Utvikling/js/firebase.js
@@ -15,26 +15,36 @@
 	
 	//Get elements
 	const preObject = document.getElementById('object');
-	const ulList = document.getElementById('history-list');
+	const ulList = document.getElementById('list');
+	
+	
+	const utgiftList= document.getElementById("utgiftList");
+	const utgiftListLengde = utgiftList.childNodes.length;
+	alert(utgiftListLengde);
+	
+	
 	
 	//CREATE REFERENCES
 	const dbRefObject = firebase.database().ref().child('object');
-	const dbRefList = dbRefObject.child('test');
+	const dbRefList = dbRefObject.child('hobbies');
 	
 	
 	//SYNCH OBJECT CHANGES: PRINTS OUT THE "OBJECT"
-	dbRefObject.on('value', snap => {
+	/*dbRefObject.on('value', snap => {
 		preObject.innerText = JSON.stringify(snap.val(), null, 3);
-	});
+	});*/
 	
 	
 	//SYNCH LIST CHANGES
 	dbRefList.on('child_added',  snap => {
 		
-		const li = document.createElement('li');
-		li.innerText = snap.val();
-		li.id = snap.key;
-		ulList.appendChild(li);
+
+		/**This works with simple list**/
+		/*$('#utgiftList').append('<li class = "ui-li-static ui-body-inherit" <li>' + snap.key + ': ' + snap.val() + '</li>');*/
+		
+		/**This works with complex list**/
+		$('#utgiftList').append('<li class = "ui-li-has-thumb"><a href="#" class = "ui-btn ui-btn-icon-right ui-icon-carat-r"><img src = "../Design/data-from-database-white.png"><p>' + snap.key + '</p><p>' + snap.val() + ',-</p></a></li>');
+		
 		
 	});
 	
